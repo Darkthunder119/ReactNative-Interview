f4a68a12e976bc94a5e09363e9a713c632149c36
Gurtaj <gurtajsingh@live.com>
2020-08-20 02:15:50 -0400

    Added all the files and working proj with env file

---
+++ InterviewApp/src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env["EXPO_API_KEY"],
+  authDomain: process.env["EXPO_AUTH_DOMAIN"],
+  databaseURL: process.env["EXPO_DATABASE_URL"],
+  projectId: process.env["EXPO_PROJECT_ID"],
+  storageBucket: process.env["EXPO_STORAGE_BUCKET"],
+  messagingSenderId: process.env["EXPO_MESSAGING_SENDER_ID"],
+  appId: process.env["EXPO_APP_ID"],
+  measurementId: process.env["EXPO_MEASUREMENT_ID"],
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
+export { firebase };
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ReactNative-Interview
+
+React Native app using EXPO CLI + Firebase 
+
+"babel-plugin-inline-dotenv": "^1.6.0" <<--required for .env files and reading the String values of the env variables. 
